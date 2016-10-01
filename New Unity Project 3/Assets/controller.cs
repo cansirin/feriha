17e84ef543df827c67fbf04a4025ee83db81cb53
Umut Sirin <umutsirin1@gmail.com>
2016-10-01 17:51:48 +0300

    first scene of the game

---
+++ New Unity Project 3/Assets/controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class controller : MonoBehaviour {
+	public int movementspeed;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.localEulerAngles = new Vector3(0,270,0);
+			transform.Translate (transform.right * movementspeed * Time.deltaTime); 
+
+		}
+		else if(Input.GetKey (KeyCode.D)) {
+			transform.localEulerAngles = new Vector3(0,90,0);
+			transform.Translate (transform.right *(-1)* movementspeed * Time.deltaTime); 
+		}
+		else if (Input.GetKey (KeyCode.W)) {
+			transform.localEulerAngles = new Vector3(0,0,0);
+			transform.Translate (transform.forward * movementspeed * Time.deltaTime); 
+		}
+		else if(Input.GetKey (KeyCode.S)) {
+			transform.localEulerAngles = new Vector3(0,180,0);
+			transform.Translate (transform.forward * (-1) * movementspeed * Time.deltaTime);
+		}
+	}
+}
---
+++ New Unity Project 3/Assets/follow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+
+	public Transform player = null;
+	public float cameraHeight = 77.0f;
+	private Transform cam = null;
+
+
+	public void Start()
+	{
+		cam = transform;
+	}
+	public void Update()
+	{
+		Vector3 pos = player.position;
+		pos.y = cameraHeight;
+		pos.x = pos.x - 31;
+		pos.z = pos.z - 37;
+		cam.position = pos;
+	}
+}
